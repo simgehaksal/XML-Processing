e5b7bfe241e2adc6df0c2f648e17255f5d79d98a
simgehaksal <simge.haksal@gmail.com>
2019-05-16 18:29:31 +0200

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,20 @@
+insert into postings values (null,'2003-01-11 10:15:00','a@abc.com','Welcome',
+'Welcome to the bulletin board',null);
+insert into postings values (null,'2003-01-12 08:00:00','b@abc.com','Welcome',
+'This is posting 2','1');
+insert into postings values (null,'2003-01-13 09:00:00','c@abc.com','Welcome',
+'This is posting 3','1');
+insert into postings values (null,'2003-01-14 09:00:00','a@abc.com','Welcome',
+'This is posting 4',null);
+insert into postings values (null,'2003-01-15 09:00:00','a@abc.com','Welcome',
+'This is posting 5',null);
+insert into postings values (null,'2003-01-16 09:00:00','a@abc.com','Welcome',
+'This is posting 6',null);
+insert into postings values (null,'2003-01-17 09:00:00','c@abc.com','Welcome',
+'This is posting 7','1:2');
+insert into postings values (null,'2003-01-18 09:00:00','d@abc.com','Welcome',
+'This is posting 8','1:2');
+insert into postings values (null,'2003-01-19 09:00:00','a@abc.com','Welcome',
+'This is posting 9','1:2:8');
+insert into postings values (null,'2003-01-20 09:00:00','b@abc.com','Welcome',
+'This is posting 10','6');
